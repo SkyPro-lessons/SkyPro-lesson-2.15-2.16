dc2dd88abad433d4fe691890e8d0daf23709085f
Tarasov Sergei <www.neon@mail.ru>
2022-10-27 13:49:55 +0500

    Initial commit

---
+++ src/test/java/site/telion/skyprolesson214myarraylist/ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.telion.skyprolesson214myarraylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
